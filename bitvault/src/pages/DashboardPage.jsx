61151b48e6d00e482f01dede3710e2b1829a6ab2
Tiqur <Tiqur7@gmail.com>
2021-02-14 00:11:49 -0800

    Added title to sidebar and fixed blur overlay

--- bitvault/src/pages/DashboardPage.jsx
+++ bitvault/src/pages/DashboardPage.jsx
@@ -2,15 +2,18 @@ import SideBar from '../components/SideBar';
 import NavBar from '../components/NavBar';
 import styled from 'styled-components';
 
-
-
+const Container = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: flex-end;
+`
 
 const DashboardPage = (props) => {
     return (
-        <>
+        <Container>
             <NavBar />
             <SideBar />
-        </>
+        </ Container>
     )
 }
 
